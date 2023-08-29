bc2b617792f72b759e26e1f5cba6e99f618d4fbb
Vincent.yu <gn870988@gmail.com>
2023-08-29 23:09:14 +0800

    End of section 5

---
+++ src/IdentityService/Services/CustomProfileService.cs
@@ -0,0 +1,43 @@
+﻿using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
+using IdentityModel;
+using IdentityService.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace IdentityService.Services;
+
+public class CustomProfileService : IProfileService
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public CustomProfileService(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+    {
+        var user = await _userManager.GetUserAsync(context.Subject);
+
+        if (user == null)
+        {
+            return;
+        }
+
+        var existingClaims = await _userManager.GetClaimsAsync(user);
+
+        var claims = new List<Claim>
+        {
+            new("username", user?.UserName ?? string.Empty)
+        };
+
+        context.IssuedClaims.AddRange(claims);
+        context.IssuedClaims.Add(existingClaims.FirstOrDefault(x => x.Type == JwtClaimTypes.Name));
+    }
+
+    public Task IsActiveAsync(IsActiveContext context)
+    {
+        return Task.CompletedTask;
+    }
+}
