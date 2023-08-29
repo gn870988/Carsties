d4837935553ac2f97b8b1050416de3917ce4b9cf
Vincent.yu <gn870988@gmail.com>
2023-08-29 12:30:33 +0800

    End of section 4

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,4 +1,6 @@
+using AuctionService.Consumers;
 using AuctionService.Data;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +12,26 @@ builder.Services.AddDbContext<AuctionDbContext>(opt =>
 });
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddMassTransit(c =>
+{
+    c.AddEntityFrameworkOutbox<AuctionDbContext>(o =>
+    {
+        o.QueryDelay = TimeSpan.FromSeconds(10);
+
+        o.UsePostgres();
+        o.UseBusOutbox();
+    });
+
+    c.AddConsumersFromNamespaceContaining<AuctionCreatedFaultConsumer>();
+
+    c.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("auction", false));
+
+    c.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var app = builder.Build();
 
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,15 +1,36 @@
+using MassTransit;
 using Polly;
 using Polly.Extensions.Http;
+using SearchService.Consumers;
 using SearchService.Data;
 using SearchService.Services;
 using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddHttpClient<AuctionSvcHttpClient>()
     .AddPolicyHandler(GetPolicy());
 
-builder.Services.AddControllers();
+builder.Services.AddMassTransit(c =>
+{
+    c.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+
+    c.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
+
+    c.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ReceiveEndpoint("search-auction-created", e =>
+        {
+            e.UseMessageRetry(r => r.Interval(5, 5));
+
+            e.ConfigureConsumer<AuctionCreatedConsumer>(context);
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var app = builder.Build();
 
