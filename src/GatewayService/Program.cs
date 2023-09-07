144931d67c5f1b73c02806f2e4f93ebfdf430e5d
Vincent.yu <gn870988@gmail.com>
2023-09-07 15:42:19 +0800

    End of section 12

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -14,8 +14,21 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         options.TokenValidationParameters.NameClaimType = "username";
     });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("customPolicy", policyBuilder =>
+    {
+        policyBuilder
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()
+            .WithOrigins(builder.Configuration["ClientApp"] ?? string.Empty);
+    });
+});
+
 var app = builder.Build();
 
+app.UseCors();
 app.MapReverseProxy();
 
 app.UseAuthentication();
